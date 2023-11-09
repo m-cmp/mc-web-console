e34d56bf22c5091b1bfd9272e789f16bfacdc215
dogfootman <idogfootman@gmail.com>
2023-11-10 08:00:15 +0900

    SetCurrentWorkspace, SetCurrentNamespace 추가

--- actions/users.go
+++ actions/users.go
@@ -14,6 +14,12 @@ import (
 
 	"mc_web_console/handler"
 	"mc_web_console/models"
+
+	middleware "mc_web_console/actions/middleware"
+	iammanager "mc_web_console/frameworkmodel/iammanager"
+	tbcommon "mc_web_console/frameworkmodel/tumblebug/common"
+
+	util "mc_web_console/util"
 )
 
 func (a actions) UsersNewForm(c buffalo.Context) error {
@@ -26,10 +32,22 @@ func (a actions) UsersNewForm(c buffalo.Context) error {
 func (a actions) MainForm(c buffalo.Context) error {
 	u := models.MCUser{}
 	c.Set("user", u)
-	if ns := c.Session().Get("current_namespace"); ns != nil {
-		c.Set("current_namespace", ns)
-	}
 
+	// c.Set("current_workspace", "-")
+	// c.Set("current_workspace_id", "--")
+	// c.Set("current_namespace", "|")
+	// c.Set("current_namespace_id", "||")
+
+	// middleware에서 user work space를 조회하고 workspace 선택 시 namespace를 조회하므로 해당 로직 필요없음.
+	// if ns := c.Session().Get("current_namespace"); ns != nil {
+	// 	log.Println("Mainform ns ", ns)
+	// 	c.Set("current_namespace", ns)
+
+	// } else {
+	// 	c.Set("current_namespace", "")
+	// 	c.Set("current_namespace_id", "")
+	// }
+	log.Println("c get xx 22222222222222222222222222")
 	return c.Render(200, r.HTML("main/index.html"))
 }
 
@@ -70,67 +88,185 @@ func (a actions) UsersCreate(c buffalo.Context) error {
 	return RedirectTool(c, "authNewForm")
 }
 
-// 기본 namespace 설정완료.
-// SetCurrentNamespace
-//
+// 현재 사용자의 workspace 설정
+func (a actions) SetCurrentWorkspace(c buffalo.Context) error {
+	log.Println("SetCurrentWorkspace")
 
-func (a actions) SetCurrentNamespace(c buffalo.Context) error {
-	log.Println("SetCurrentNamespace")
-	spew.Dump("======setCurrenttNamespace======")
-	namespaceID := c.Param("nsId")
-	log.Println(namespaceID)
-
-	//ns, ns_err := handler.GetNamespaceById(namespaceID)
-	// namespaceName := ns.NsName
-	// c.Session().Set("current_namespace_id", namespaceID)
-	// c.Session().Set("current_namespace", namespaceName)
-	// c.Set("current_namespace_id", namespaceID)
-	// c.Set("current_namespace", namespaceName)
-	// namespace가 해당 user에게 할당되어 있으면 current_namespace로 설정한다
-
-	log.Println(c.Session().Get("current_user_id"))
-	uid := c.Session().Get("current_user_id").(uuid.UUID)
-	if uid == uuid.Nil {
-		return c.Render(301, r.JSON(map[string]interface{}{
-			"error":  "user session doesn't exist",
-			"status": "301",
-		}))
+	//WorkspaceID
+	mcimWorkspace := &iammanager.MCIamWorkspace{}
+	if err := c.Bind(mcimWorkspace); err != nil {
+		return c.Render(http.StatusBadRequest, r.JSON(err))
 	}
+	workspaceID := mcimWorkspace.ID
+	//workspaceID := c.Param("wsId")// Get일 때
+	log.Println("workspaceID ", workspaceID)
 
-	// 에러 : ERRO[2023-11-03T17:16:27+09:00] interface conversion: interface {} is nil, not *models.MCUser
-	//currentUser := c.Session().Get("current_user").(*models.MCUser)
-	//log.Println("currentUser ", currentUser)
+	// session에서 사용자 정보 꺼냄.
+	userInfo, err := middleware.GetUserInfoFromSession(c)
 
-	tx := c.Value("tx").(*pop.Connection)
-
-	// user, _ := handler.GetUserById(uid)
-	// log.Println("user ", user)
-	//exists, userNamespace := handler.CheckExistsUserNamespace(uid, namespaceID, tx)
-	exists, _ := handler.CheckExistsUserNamespace(uid, namespaceID, tx)
-	if !exists {
-		return c.Render(301, r.JSON(map[string]interface{}{
-			"error":  "cannot find the user's namespace of user",
-			"status": "301",
-		}))
-	}
-	// log.Println("UserNamespace ", userNamespace)
-
-	// Model에 관계정의를 하면 자동으로 Join이 되어야 하는데... 안되어서 ns만 다시 조회
-	ns, err := handler.GetNamespaceById(namespaceID)
 	if err != nil {
+		log.Println("GetUserInfoFromSession failed at SetCurrentWorkspace ")
+		return c.Render(http.StatusUnauthorized, r.JSON(map[string]interface{}{
+			"error":  err,
+			"status": http.StatusUnauthorized,
+		}))
+	}
+
+	// user의 workspace 목록 조회
+	// Iam Manager로 로그인
+	if util.USE_MCIAM == "Y" {
+		// DB에서 Token 조회
+		authSession := &models.AuthSession{}
+		tx := c.Value("tx").(*pop.Connection)
+		err := tx.Where("mcuser_id = ?", userInfo.UserID).First(authSession)
+		if err != nil {
+			return c.Render(http.StatusBadRequest, r.JSON(map[string]interface{}{
+				"error": err,
+			}))
+		}
+		log.Println(authSession)
+		log.Println("Get user workspaces")
+		// iam manager에서 workspace 조회
+		workspace, respStatus := handler.GetIamManagerWorkspaceByID(authSession.IamManagerAccessToken, workspaceID)
+		log.Println("respStatus", respStatus)
+		if respStatus.StatusCode != 200 {
+			return c.Render(http.StatusBadRequest, r.JSON(map[string]interface{}{
+				"error":  respStatus.Message,
+				"status": respStatus.StatusCode,
+			}))
+		}
+		if workspace.ID == "" {
+			log.Println("workspace doesn't exist")
+			return c.Render(http.StatusBadRequest, r.JSON(map[string]interface{}{
+				"error":  respStatus.Message,
+				"status": respStatus.StatusCode,
+			}))
+		}
+		log.Println(workspace)
+		userInfo.CurrentWorkspaceID = workspace.ID
+		userInfo.CurrentWorkspaceName = workspace.Name
+		userInfo.CurrentNamespaceID = ""   // workspace변경이되면 namespace는 초기화
+		userInfo.CurrentNamespaceName = "" // workspace변경이되면 namespace는 초기화
+		middleware.SetUserSession(c, userInfo)
+	} else {
+		// MCIAM을 사용해야 workspace를 이용할 수 있음.
 		return c.Render(301, r.JSON(map[string]interface{}{
-			"error":  "cannot find the user's namespace of user",
+			"error":  "Please activate MCIAM-Manager ",
 			"status": "301",
 		}))
 	}
-	log.Println("UserNamespace ns ", ns)
-	c.Session().Set("current_namespace_id", ns.ID)
-	c.Session().Set("current_namespace", ns.NsName)
-	c.Set("current_namespace_id", ns.ID)
-	c.Set("current_namespace", ns.NsName)
 
 	return c.Render(200, r.JSON(map[string]interface{}{
-		"CurrentNamespaceID": ns.NsName,
+		"CurrentWorkspaceID":   userInfo.CurrentWorkspaceID,
+		"CurrentWorkspaceName": userInfo.CurrentWorkspaceName,
+		"CurrentNamespaceID":   userInfo.CurrentNamespaceID,
+		"CurrentNamespaceName": userInfo.CurrentNamespaceName,
+	}))
+}
+
+// 기본 namespace 설정완료.
+// SetCurrentNamespace
+// TODO : 이름도 Namespace -> project로 바꿔야 하나??
+func (a actions) SetCurrentNamespace(c buffalo.Context) error {
+	log.Println("SetCurrentNamespace")
+
+	//namespaceID := c.Param("nsId")
+	//log.Println(namespaceID)
+	namespaceInfo := &tbcommon.TbNsInfo{}
+	if err := c.Bind(namespaceInfo); err != nil {
+		return c.Render(http.StatusBadRequest, r.JSON(err))
+	}
+
+	// session에서 사용자 정보 꺼냄.
+	userInfo, err := middleware.GetUserInfoFromSession(c)
+	if err != nil {
+		log.Println("GetUserInfoFromSession failed at SetCurrentNamespace ")
+		return c.Render(http.StatusUnauthorized, r.JSON(map[string]interface{}{
+			"error":  err,
+			"status": http.StatusUnauthorized,
+		}))
+	}
+
+	// mciam manager를 사용하는 경우 workspace 가 먼저 선택되어야 한다.
+	if util.USE_MCIAM == "Y" {
+		// workspace가 선택되어 있어야 한다.
+		if userInfo.CurrentWorkspaceID == "" {
+			return c.Render(http.StatusUnauthorized, r.JSON(map[string]interface{}{
+				"error":  "Select the workspace first",
+				"status": http.StatusUnauthorized,
+			}))
+		}
+
+		// DB에서 Token 조회
+		authSession := &models.AuthSession{}
+		tx := c.Value("tx").(*pop.Connection)
+		err := tx.Where("mcuser_id = ?", userInfo.UserID).First(authSession)
+		if err != nil {
+			return c.Render(http.StatusBadRequest, r.JSON(map[string]interface{}{
+				"error": err,
+			}))
+		}
+		log.Println(authSession)
+
+		// 해당 workspace내 project가 존재해야 한다.( iam 전송시 namespace => project로 사용 )// iammanager.MCIamProject
+		project, respStatus := handler.GetIamManagerProject(authSession.IamManagerAccessToken, userInfo.CurrentWorkspaceID, namespaceInfo.ID)
+		log.Println("respStatus", respStatus)
+		if respStatus.StatusCode != 200 {
+			return c.Render(http.StatusBadRequest, r.JSON(map[string]interface{}{
+				"error":  respStatus.Message,
+				"status": respStatus.StatusCode,
+			}))
+		}
+		if project.ID != "" {
+			log.Println("namespace doesn't exist")
+			return c.Render(http.StatusBadRequest, r.JSON(map[string]interface{}{
+				"error":  respStatus.Message,
+				"status": respStatus.StatusCode,
+			}))
+		}
+
+		userInfo.CurrentNamespaceID = project.ID
+		userInfo.CurrentNamespaceName = project.Name
+		middleware.SetUserSession(c, userInfo)
+	} else {
+		namespaceID := namespaceInfo.ID
+		uid, err := uuid.FromString(userInfo.UserID) // 사용자 ID를 DB에서 조회결과를 넣었으면 조회도 uuid형태로 변경필요.
+		if err != nil {
+			log.Println("userID  ", userInfo.UserID)
+			log.Println("can not change to uuid ", err)
+			return c.Render(500, r.JSON(map[string]interface{}{
+				"error":  err.Error(),
+				"status": 500,
+			}))
+		}
+
+		tx := c.Value("tx").(*pop.Connection)
+		exists, _ := handler.CheckExistsUserNamespace(uid, namespaceID, tx)
+		if !exists {
+			return c.Render(301, r.JSON(map[string]interface{}{
+				"error":  "cannot find the user's namespace of user",
+				"status": "301",
+			}))
+		}
+		// log.Println("UserNamespace ", userNamespace)
+
+		// Model에 관계정의를 하면 자동으로 Join이 되어야 하는데... 안되어서 ns만 다시 조회
+		namespace, err := handler.GetNamespaceById(namespaceID)
+		if err != nil {
+			return c.Render(301, r.JSON(map[string]interface{}{
+				"error":  "cannot find the user's namespace of user",
+				"status": "301",
+			}))
+		}
+
+		userInfo.CurrentNamespaceID = namespace.ID
+		userInfo.CurrentNamespaceName = namespace.NsName // local table에 ns이름을 NsName으로 정의 함.
+		middleware.SetUserSession(c, userInfo)
+	}
+
+	return c.Render(200, r.JSON(map[string]interface{}{
+		"CurrentNamespaceID":   userInfo.CurrentNamespaceID,
+		"CurrentNamespaceName": userInfo.CurrentNamespaceName,
 	}))
 }
 
