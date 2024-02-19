098ee5c6ee5ce04a7f76dc6d9a020e5fa0559253
raccoon-pi <wsx1341@gmail.com>
2024-02-19 07:01:35 +0000

    init

--- assets/js/application.js
+++
@@ -1,9 +0,0 @@
-//require("expose-loader?exposes=$,jQuery!jquery");
-// require("bootstrap/dist/js/bootstrap.js");
-// require("bootstrap/js/dist/modal.js");
-require("@fortawesome/fontawesome-free/js/all.js");
-//require("jquery-ujs/src/rails.js");
-
-$(() => {
-
-});
