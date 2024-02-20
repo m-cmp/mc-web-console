e7b3378cc30bbb5314ce0328f6d037ef148ee344
raccoon-pi <wsx1341@gmail.com>
2024-02-20 08:18:20 +0000

    init tabler

---
+++ mc_web_console_front/assets/js/application.js
@@ -0,0 +1,26 @@
+require("expose-loader?exposes=$,jQuery!jquery");
+require("bootstrap/dist/js/bootstrap.bundle.js");
+require("@fortawesome/fontawesome-free/js/all.js");
+require("jquery-ujs/src/rails.js");
+
+// require("@tabler/core/dist/js/tabler.js");
+// require("@tabler/core/dist/js/demo.js");
+
+// require("./tabler/demo")
+// require("./tabler/demo-theme")
+// require("./tabler/tabler")
+
+// import apexcharts
+require("apexcharts/dist/apexcharts.min.js");
+
+// import jsvectormap
+require("jsvectormap/dist/js/jsvectormap.min.js");
+require("jsvectormap/dist/maps/world.js");
+require("jsvectormap/dist/maps/world-merc.js");
+
+// import litepicker
+require("litepicker/dist/litepicker.js");
+
+$(() => {
+
+});
---
+++ mc_web_console_front/assets/js/pages/default-test.js
@@ -0,0 +1,13 @@
+// this is start and test copy js
+
+import axios from 'axios'
+
+document.addEventListener("DOMContentLoaded", function () {
+    axios.get('http://ifconfig.me/all.json')
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            console.error(error);
+        });
+});
