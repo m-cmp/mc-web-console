510e42643760577e9dfdc477a7f6c49e280da822
raccoon-mh <wsx1341@gmail.com>
2024-02-25 15:16:06 +0900

    fix build embed error

--- mc_web_console_front/templates/embed.go
+++ mc_web_console_front/templates/embed.go
@@ -2,14 +2,23 @@ package templates
 
 import (
 	"embed"
+	"fmt"
 	"io/fs"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/gobuffalo/buffalo"
 )
 
-//go:embed */*/*/*
+// ???????????? TODO : 정규식 표현이 이상함...
+//
+//go:embed *.html
+//go:embed */*.html
+//go:embed */*/*.html
+//go:embed */*/*/*.html
 var files embed.FS
 
 func FS() fs.FS {
+	fmt.Println(files)
+	spew.Dump(files)
 	return buffalo.NewFS(files, "templates")
 }
