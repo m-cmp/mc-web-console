0a866dcc5bb5cb2132957cd851ed5fbb653980d1
SungWoongz <ftsw0221@naver.com>
2024-04-19 13:11:55 +0900

    Add cluster handler & controller

--- mc_web_console_api/actions/app.go
+++ mc_web_console_api/actions/app.go
@@ -68,7 +68,126 @@ func App() *buffalo.App {
 		api.GET("/{path:.+}", GetRouteController)
 		api.POST("/{path:.+}", PostRouteController)
 
-		// DEBUG START //
+		// API TEST
+		setting := app.Group(apiPath + "/setting")
+		namespaces := setting.Group("/namespaces")
+		namespace := namespaces.Group("/namespace")
+		namespace.GET("/list", NamespaceAllList)
+		namespace.POST("/reg/proc", NamespaceReg)
+
+		lu := app.Group(apiPath + "/lu")
+		lu.POST("/setting/resources/machineimage/lookupimage", LookupVirtualMachineImageData)
+
+		// List all configs
+		config := app.Group(apiPath + "/config")
+		config.GET("/", TbConfig)
+
+		// Get config
+		// configByid := app.Group(apiPath + "/config")
+		// configByid.GET("/", TBconfigbyId)
+
+		// loadcommonresource
+		loadcommonresource := app.Group(apiPath + "loadcommonresource")
+		loadcommonresource.GET("/", LoadCommonResource)
+
+		// [Admin] Multi-Cloud Environment Configuration
+		health := app.Group(apiPath + "/health")
+		health.GET("/", GetTBHealth)
+
+		// GET CSP Resources Overview
+		resources := setting.Group("/resources")
+		resources.GET("/inspectresourcesoverview", GetInspectResourcesOverview)
+
+		// Get value of an object
+		object := app.Group(apiPath + "/object")
+		object.GET("/", GetObjectThroughTB)
+
+		// List all objects for a given key
+		objects := app.Group(apiPath + "/objects")
+		objects.GET("/", GetObjectListThroughTB)
+
+		// [Infra service] MCIS Provisioning management
+		operation := app.Group(apiPath + "/operation")
+		manages := operation.Group("/manages")
+		mcismng := manages.Group("/mcismng")
+
+		// List all MCISs or MCISs' ID
+		mcismng.GET("/list", McisList)
+		// Get MCIS, Action to MCIS (status, suspend, resume, reboot, terminate, refine), or Get VMs' ID
+		//mcismng.GET("/", McisGet)
+
+		ns := app.Group(apiPath + "/ns")
+		nsid := ns.Group("/{nsid}")
+		mciss := nsid.Group("/mcis")
+		mcisid := mciss.Group("/{mcisid}")
+		mcisid.GET("/", McisGet)
+
+		// List SubGroup IDs in a specified MCIS
+		mcismng.GET("/{mcisid}"+"/subgroup", McisSubGroupList)
+
+		mcis := app.Group(apiPath + "/mcis")
+		mcis.GET("/mcislist", McisList)
+
+		// for the test
+		cluster := app.Group(apiPath + "/cluster")
+		cluster.GET("/ns/{nsId}/cluster", ClusterList)
+		cluster.POST("/ns/{nsId}/cluster", ClusterReg)
+		cluster.DELETE("/ns/{nsId}/cluster/{clusterId}", DelCluster)
+		cluster.GET("/ns/{nsId}/cluster/{clusterId}", ClusterList)
+		cluster.POST("/ns/{nsId}/cluster/{clusterId}/nodegroup", RegNodeGroup)
+		//////////////////////////////////////////////////////////////////////
+		// TOBE PATH
+
+		// ns
+
+		// ns/{nsid}
+
+		// benchmark
+
+		// cluster
+
+		// cmd
+
+		// control
+
+		// mcis
+
+		// mcis/{mcisid}/nlb
+
+		// mcis/{mcisid}/subgroup
+
+		// mcis/{mcisid}/vm
+
+		// monitoring
+
+		// network
+
+		// policy/mcis
+
+		// resources/customImage
+
+		// resources/dataDisk
+
+		// resources/
+
+		// resources/image
+
+		// resources/securityGroup
+
+		// resources/spec
+
+		// resources/sshKey
+
+		// resources/vNET
+
+		// object
+
+		// objects
+
+		//
+		// MC-IAM-MANAGER REQUIRERD
+
+		// DEBUG START //api/debug/tumblebug
 		if ENV == "development" {
 			debug := app.Group(apiPath + "/debug")
 			debug.ANY("/{targetfw}/{path:.+}", DebugApiCaller)
--- mc_web_console_api/actions/clustermng.go
+++ mc_web_console_api/actions/clustermng.go
@@ -1,1 +1,208 @@
 package actions
+
+import (
+	"log"
+	"mc_web_console_api/fwmodels/tumblebug/mcis"
+	"mc_web_console_api/handler"
+	"mc_web_console_api/util"
+	"net/http"
+
+	"github.com/gobuffalo/buffalo"
+)
+
+func ClusterList(c buffalo.Context) error {
+	namespaceID := c.Param("nsId")
+
+	optionParam := c.Params().Get("option")
+	log.Println("=========clusterlist")
+	if optionParam == "id" {
+		clusterList, respStatus := handler.ClusterIDList(namespaceID)
+		if respStatus.StatusCode != 200 && respStatus.StatusCode != 201 {
+			return c.Render(respStatus.StatusCode, r.JSON(map[string]interface{}{
+				"error":  respStatus.Message,
+				"status": respStatus.StatusCode,
+			}))
+		}
+
+		return c.Render(http.StatusOK, r.JSON(map[string]interface{}{
+			"message":     "success",
+			"status":      respStatus.StatusCode,
+			"ClusterList": clusterList,
+		}))
+	} else {
+		clusterList, respStatus := handler.ClusterList(namespaceID, optionParam)
+		if respStatus.StatusCode != 200 && respStatus.StatusCode != 201 {
+			return c.Render(respStatus.StatusCode, r.JSON(map[string]interface{}{
+				"error":  respStatus.Message,
+				"status": respStatus.StatusCode,
+			}))
+		}
+
+		return c.Render(http.StatusOK, r.JSON(map[string]interface{}{
+			"message":     "success",
+			"status":      respStatus.StatusCode,
+			"ClusterList": clusterList,
+		}))
+	}
+}
+
+func ClusterReg(c buffalo.Context) error {
+	namespaceID := c.Param("nsId")
+	optionParam := c.Params().Get("option")
+	clusterReq := &mcis.TbClusterReq{}
+	if err := c.Bind(clusterReq); err != nil {
+		return c.Render(http.StatusBadRequest, r.JSON(err))
+	}
+
+	go handler.CreateCluster(namespaceID, optionParam, clusterReq)
+	// 원래는 호출 결과를 return하나 go routine으로 바꾸면서 요청성공으로 return
+	return c.Render(http.StatusOK, r.JSON(map[string]interface{}{
+		"message": "success",
+		"status":  200,
+	}))
+}
+
+func DelCluster(c buffalo.Context) error {
+	namespaceID := c.Param("nsId")
+	clusterID := c.Param("clusterId")
+
+	taskKey := namespaceID + "||" + "mcks" + "||" + clusterID
+	handler.StoreWebsocketMessage(util.TASK_TYPE_MCKS, taskKey, util.TUMBLEBUG, util.TASK_STATUS_REQUEST, c) // session에 작업내용 저장
+
+	go handler.DelCluster(namespaceID, clusterID)
+
+	return c.Render(http.StatusOK, r.JSON(map[string]interface{}{
+		"message": "success",
+		"status":  200,
+	}))
+}
+
+func RegNodeGroup(c buffalo.Context) error {
+
+	namespaceID := c.Param("nsId")
+
+	clusterName := c.Param("mcksName")
+
+	nodeRegReq := &mcis.TbNodeGroupReq{}
+	if err := c.Bind(nodeRegReq); err != nil {
+		return c.Render(http.StatusBadRequest, r.JSON(map[string]interface{}{
+			"message": "fail",
+			"status":  "fail",
+		}))
+	}
+
+	nodeInfo, respStatus := handler.AddNodeGroup(namespaceID, clusterName, nodeRegReq)
+	log.Println("RegNode service returned")
+	if respStatus.StatusCode != 200 && respStatus.StatusCode != 201 {
+		return c.Render(respStatus.StatusCode, r.JSON(map[string]interface{}{
+			"error":  respStatus.Message,
+			"status": respStatus.StatusCode,
+		}))
+	}
+
+	return c.Render(http.StatusOK, r.JSON(map[string]interface{}{
+		"message":       "success",
+		"status":        respStatus.StatusCode,
+		"NodeGroupInfo": nodeInfo,
+	}))
+}
+
+// del all cluster
+func DelAllCluster(c buffalo.Context) error {
+	namespaceID := c.Param("nsId")
+	matchParam := c.Params().Get("match")
+
+	go handler.DelAllCluster(namespaceID, matchParam)
+
+	return c.Render(http.StatusOK, r.JSON(map[string]interface{}{
+		"message": "success",
+		"status":  200,
+	}))
+}
+
+// remove a nodegroup
+func DelNodeGroup(c buffalo.Context) error {
+
+	namespaceID := c.Param("nsId")
+	clusterID := c.Param("clusterId")
+	nodeGroupName := c.Param("nodeGroupName")
+
+	resultStatusInfo, respStatus := handler.DelNodeGroup(namespaceID, clusterID, nodeGroupName)
+	log.Println("DelNodeGroup service returned")
+	if respStatus.StatusCode != 200 && respStatus.StatusCode != 201 {
+		return c.Render(http.StatusBadRequest, r.JSON(map[string]interface{}{
+			"error":  respStatus.Message,
+			"status": respStatus.StatusCode,
+		}))
+	}
+
+	return c.Render(http.StatusOK, r.JSON(map[string]interface{}{
+		"message":    "success",
+		"status":     respStatus.StatusCode,
+		"StatusInfo": resultStatusInfo,
+	}))
+}
+
+// change a nodegroup's autoscale size
+func EditNodeGroupAutoscalesize(c buffalo.Context) error {
+	nameSpaceID := c.Param("nsId")
+	clusterID := c.Param("clusterId")
+	nodeGroupName := c.Param("nodeGroupName")
+
+	resultNodegroupAutoscalesize, respStatus := handler.EditNodeGroupAutoscalesize(nameSpaceID, clusterID, nodeGroupName)
+	if respStatus.StatusCode != 200 && respStatus.StatusCode != 201 {
+		return c.Render(respStatus.StatusCode, r.JSON(map[string]interface{}{
+			"error":  respStatus.Message,
+			"status": respStatus.StatusCode,
+		}))
+	}
+	return c.Render(http.StatusOK, r.JSON(map[string]interface{}{
+		"message":    "success",
+		"status":     respStatus.StatusCode,
+		"StatusInfo": resultNodegroupAutoscalesize,
+	}))
+
+}
+
+// set a nodegrouop's autoscaling on/off
+
+func EditAutoscaling(c buffalo.Context) error {
+	nameSpaceID := c.Param("nsId")
+	clusterID := c.Param("clusterId")
+	nodeGroupName := c.Param("nodeGroupName")
+
+	resultAutoscaling, respStatus := handler.EditAutoscaling(nameSpaceID, clusterID, nodeGroupName)
+	if respStatus.StatusCode != 200 && respStatus.StatusCode != 201 {
+		return c.Render(respStatus.StatusCode, r.JSON(map[string]interface{}{
+			"error":  respStatus.Message,
+			"status": respStatus.StatusCode,
+		}))
+	}
+	return c.Render(http.StatusOK, r.JSON(map[string]interface{}{
+		"message":    "success",
+		"status":     respStatus.StatusCode,
+		"StatusInfo": resultAutoscaling,
+	}))
+
+}
+
+// upgrage a cluster's version
+
+func EditClusterVersion(c buffalo.Context) error {
+	nameSpaceID := c.Param("nsId")
+	clusterID := c.Param("clusterId")
+
+	resultClusterVersion, respStatus := handler.UpgradeClusterVersion(nameSpaceID, clusterID)
+	if respStatus.StatusCode != 200 && respStatus.StatusCode != 201 {
+		return c.Render(respStatus.StatusCode, r.JSON(map[string]interface{}{
+			"error":  respStatus.Message,
+			"status": respStatus.StatusCode,
+		}))
+	}
+	return c.Render(http.StatusOK, r.JSON(map[string]interface{}{
+		"message":    "success",
+		"status":     respStatus.StatusCode,
+		"StatusInfo": resultClusterVersion,
+	}))
+
+}
