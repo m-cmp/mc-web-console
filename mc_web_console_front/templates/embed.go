32bd2dd8fdeb7a10d3daa20fec3587ae7c3ad248
raccoon-pi <wsx1341@gmail.com>
2024-04-26 02:25:32 +0000

    restruct init

--- mc_web_console_front/templates/embed.go
+++ mc_web_console_front/templates/embed.go
@@ -7,12 +7,6 @@ import (
 	"github.com/gobuffalo/buffalo"
 )
 
-// ???????????? TODO : 정규식 표현이 이상함...
-//
-//go:embed *.html
-//go:embed */*.html
-//go:embed */*/*.html
-//go:embed */*/*/*.html
 var files embed.FS
 
 func FS() fs.FS {
