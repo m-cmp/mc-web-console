0257071127d2993c54b3a266e06768b249506174
SungWoongz <ftsw0221@naver.com>
2024-05-21 09:44:19 +0900

    develop APIs Integrated Release

--- mc_web_console_api/actions/tumblebug/mcismng.go
+++ mc_web_console_api/actions/tumblebug/mcismng.go
@@ -31,3 +31,45 @@ func CreateMCIS(c buffalo.Context, commonRequest *webconsole.CommonRequest) *web
 	commonResponse, _ := webconsole.CommonCaller(http.MethodPost, util.TUMBLEBUG, endPoint, commonRequest, webconsole.TBAuthentication())
 	return commonResponse
 }
+
+func CreateDynamicMCIS(c buffalo.Context, commonRequest *webconsole.CommonRequest) *webconsole.CommonResponse {
+	endPoint := "/ns/{nsId}/mcisDynamic"
+	commonResponse, _ := webconsole.CommonCaller(http.MethodPost, util.TUMBLEBUG, endPoint, commonRequest, webconsole.TBAuthentication())
+	return commonResponse
+}
+
+func GetLoadDefaultResouce(c buffalo.Context, commonRequest *webconsole.CommonRequest) *webconsole.CommonResponse {
+	endPoint := "/ns/{nsId}/loadDefaultResource"
+	commonResponse, _ := webconsole.CommonCaller(http.MethodGet, util.TUMBLEBUG, endPoint, commonRequest, webconsole.TBAuthentication())
+	return commonResponse
+}
+
+func DelDefaultResouce(c buffalo.Context, commonRequest *webconsole.CommonRequest) *webconsole.CommonResponse {
+	endPoint := "/ns/{nsId}/defaultResources"
+	commonResponse, _ := webconsole.CommonCaller(http.MethodDelete, util.TUMBLEBUG, endPoint, commonRequest, webconsole.TBAuthentication())
+	return commonResponse
+}
+
+func MCISRecommendVm(c buffalo.Context, commonRequest *webconsole.CommonRequest) *webconsole.CommonResponse {
+	endPoint := "/mcisRecommendVm"
+	commonResponse, _ := webconsole.CommonCaller(http.MethodPost, util.TUMBLEBUG, endPoint, commonRequest, webconsole.TBAuthentication())
+	return commonResponse
+}
+
+func MCISDynamicCheckRequest(c buffalo.Context, commonRequest *webconsole.CommonRequest) *webconsole.CommonResponse {
+	endPoint := "/mcisDynamicCheckRequest"
+	commonResponse, _ := webconsole.CommonCaller(http.MethodPost, util.TUMBLEBUG, endPoint, commonRequest, webconsole.TBAuthentication())
+	return commonResponse
+}
+
+func SendCommandtoMCIS(c buffalo.Context, commonRequest *webconsole.CommonRequest) *webconsole.CommonResponse {
+	endPoint := "/ns/{nsId}/cmd/mcis/{mcisId}"
+	commonResponse, _ := webconsole.CommonCaller(http.MethodPost, util.TUMBLEBUG, endPoint, commonRequest, webconsole.TBAuthentication())
+	return commonResponse
+}
+
+func ControlLifecycle(c buffalo.Context, commonRequest *webconsole.CommonRequest) *webconsole.CommonResponse {
+	endPoint := "/ns/{nsId}/control/mcis/{mcisId}"
+	commonResponse, _ := webconsole.CommonCaller(http.MethodGet, util.TUMBLEBUG, endPoint, commonRequest, webconsole.TBAuthentication())
+	return commonResponse
+}
