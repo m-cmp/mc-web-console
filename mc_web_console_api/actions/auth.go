d9a76fefc34f08bea65415951ff71f6fdcac091c
raccoon-pi <wsx1341@gmail.com>
2024-06-19 06:24:19 +0000

    logout success

--- mc_web_console_api/actions/auth.go
+++ mc_web_console_api/actions/auth.go
@@ -5,6 +5,7 @@ import (
 	"mc_web_console_api/handler/mciammanager"
 	"mc_web_console_api/handler/self"
 	"net/http"
+	"strings"
 
 	"github.com/gobuffalo/buffalo"
 	"github.com/gobuffalo/pop/v6"
@@ -22,10 +23,11 @@ func AuthLogin(c buffalo.Context) error {
 		}
 
 		tx := c.Value("tx").(*pop.Connection)
-		_, err := self.CreateUserSessFromResponseData(tx, commonResponse, commonRequest.Request.(map[string]interface{})["id"].(string))
+		sess, err := self.CreateUserSessFromResponseData(tx, commonResponse, commonRequest.Request.(map[string]interface{})["id"].(string))
 		if err != nil {
 			return c.Render(http.StatusInternalServerError, r.JSON(map[string]interface{}{"error": err.Error()}))
 		}
+		c.Session().Set("Authorization", sess.AccessToken)
 		return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
 	} else {
 		commonResponse = handler.CommonResponseStatusInternalServerError("NOT IMPL")
@@ -53,17 +55,28 @@ func AuthLogout(c buffalo.Context) error {
 	c.Bind(commonRequest)
 
 	if MCIAM_USE {
-		// rt, err := self.DestroyUserSessByAccesstokenforLogout(tx)
-
-		// {
-		// 	"refresh_token":"{{user_refresh_token}}"
-		// }
+		accessToken := strings.TrimPrefix(c.Request().Header.Get("Authorization"), "Bearer ")
+		tx := c.Value("tx").(*pop.Connection)
+		rt, err := self.DestroyUserSessByAccesstokenforLogout(tx, accessToken)
+		if err != nil {
+			return c.Render(http.StatusInternalServerError, r.JSON(map[string]interface{}{"error": err.Error()}))
+		}
+		commonRequest := &handler.CommonRequest{
+			Request: map[string]string{
+				"refresh_token": rt,
+			},
+		}
 		commonResponse = mciammanager.McIamLogout(c, commonRequest)
+		if commonResponse.Status.StatusCode != 200 && commonResponse.Status.StatusCode != 201 {
+			return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
+		}
+		c.Session().Clear()
+		return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
 	} else {
 		commonResponse = handler.CommonResponseStatusInternalServerError("NOT IMPL")
+		return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
 	}
 
-	return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
 }
 
 func AuthGetUserInfo(c buffalo.Context) error {
