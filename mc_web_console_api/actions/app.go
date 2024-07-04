e58d0559cadc6dadd7b124d28e2aae6d765808a7
raccoon-pi <wsx1341@gmail.com>
2024-07-04 07:18:16 +0000

    route by api.yaml from admincli 🚀 :)

--- mc_web_console_api/actions/app.go
+++ mc_web_console_api/actions/app.go
@@ -49,30 +49,11 @@ func App() *buffalo.App {
 
 		auth := app.Group(apiPath + "/auth")
 		auth.POST("/login", AuthLogin)
-		// auth.POST("/login/refresh", AuthLoginRefresh)
-		auth.POST("/logout", AuthLogout)
-		// auth.GET("/userinfo", AuthGetUserInfo)
+		auth.POST("/logout", session("")(AuthLogout))
 
 		api := app.Group(apiPath)
 		api.Use(session(""))
-		api.GET("/{targetController}", GetRouteController)
-		api.POST("/{targetController}", PostRouteController)
-
-		role := app.Group("/test/role")
-		role.Use(session(""))
-		role.ANY("/alive", alive)
-
-		admin := app.Group("/test/admin")
-		admin.Use(session("admin"))
-		admin.ANY("/alive", alive)
-
-		viewer := app.Group("/test/viewer")
-		viewer.Use(session("admin"))
-		viewer.ANY("/alive", alive)
-
-		operator := app.Group("/test/operator")
-		operator.Use(session("admin"))
-		operator.ANY("/alive", alive)
+		api.POST("/{operationId}", AnyController)
 	})
 
 	return app
--- mc_web_console_api/actions/auth.go
+++ mc_web_console_api/actions/auth.go
@@ -2,7 +2,6 @@ package actions
 
 import (
 	"mc_web_console_api/handler"
-	"mc_web_console_api/handler/mciammanager"
 	"mc_web_console_api/handler/self"
 	"net/http"
 	"strings"
@@ -12,83 +11,51 @@ import (
 )
 
 func AuthLogin(c buffalo.Context) error {
-	commonResponse := &handler.CommonResponse{}
 	commonRequest := &handler.CommonRequest{}
 	c.Bind(commonRequest)
 
-	if MCIAM_USE {
-		commonResponse = mciammanager.McIamLogin(c, commonRequest)
-		if commonResponse.Status.StatusCode != 200 && commonResponse.Status.StatusCode != 201 {
-			return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
-		}
-
-		tx := c.Value("tx").(*pop.Connection)
-		_, err := self.CreateUserSessFromResponseData(tx, commonResponse, commonRequest.Request.(map[string]interface{})["id"].(string))
-		if err != nil {
-			return c.Render(http.StatusInternalServerError, r.JSON(map[string]interface{}{"error": err.Error()}))
-		}
-		// c.Session().Set("Authorization", sess.AccessToken)
-		return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
-	} else {
-		commonResponse = handler.CommonResponseStatusInternalServerError("NOT IMPL")
+	commonResponse, _ := AnyCaller(c, "login", commonRequest, false)
+	if commonResponse.Status.StatusCode != 200 && commonResponse.Status.StatusCode != 201 {
 		return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
 	}
+
+	tx := c.Value("tx").(*pop.Connection)
+	_, err := self.CreateUserSessFromResponseData(tx, commonResponse, commonRequest.Request.(map[string]interface{})["id"].(string))
+	if err != nil {
+		return c.Render(http.StatusInternalServerError, r.JSON(map[string]interface{}{"error": err.Error()}))
+	}
+
+	return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
+
 }
 
 func AuthLoginRefresh(c buffalo.Context) error {
-	commonResponse := &handler.CommonResponse{}
 	commonRequest := &handler.CommonRequest{}
 	c.Bind(commonRequest)
-
-	if MCIAM_USE {
-		commonResponse = mciammanager.McIamLoginRefresh(c, commonRequest)
-	} else {
-		commonResponse = handler.CommonResponseStatusInternalServerError("NOT IMPL")
-	}
-
+	commonResponse, _ := AnyCaller(c, "loginrefresh", commonRequest, true)
 	return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
 }
 
 func AuthLogout(c buffalo.Context) error {
-	commonResponse := &handler.CommonResponse{}
-	commonRequest := &handler.CommonRequest{}
-	c.Bind(commonRequest)
-
-	if MCIAM_USE {
-		accessToken := strings.TrimPrefix(c.Request().Header.Get("Authorization"), "Bearer ")
-		tx := c.Value("tx").(*pop.Connection)
-		rt, err := self.DestroyUserSessByAccesstokenforLogout(tx, accessToken)
-		if err != nil {
-			return c.Render(http.StatusInternalServerError, r.JSON(map[string]interface{}{"error": err.Error()}))
-		}
-		commonRequest := &handler.CommonRequest{
-			Request: map[string]string{
-				"refresh_token": rt,
-			},
-		}
-		commonResponse = mciammanager.McIamLogout(c, commonRequest)
-		if commonResponse.Status.StatusCode != 200 && commonResponse.Status.StatusCode != 201 {
-			return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
-		}
-		// c.Session().Clear()
-		return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
-	} else {
-		commonResponse = handler.CommonResponseStatusInternalServerError("NOT IMPL")
+	accessToken := strings.TrimPrefix(c.Request().Header.Get("Authorization"), "Bearer ")
+	tx := c.Value("tx").(*pop.Connection)
+	rt, err := self.DestroyUserSessByAccesstokenforLogout(tx, accessToken)
+	if err != nil {
+		commonResponse := handler.CommonResponseStatusBadRequest(err.Error())
 		return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
 	}
-
+	commonRequest := &handler.CommonRequest{
+		Request: map[string]string{
+			"refresh_token": rt,
+		},
+	}
+	commonResponse, _ := AnyCaller(c, "logout", commonRequest, true)
+	return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
 }
 
 func AuthGetUserInfo(c buffalo.Context) error {
-	commonResponse := &handler.CommonResponse{}
 	commonRequest := &handler.CommonRequest{}
 	c.Bind(commonRequest)
-
-	if MCIAM_USE {
-		commonResponse = mciammanager.McIamGetUserInfo(c, commonRequest)
-	} else {
-		commonResponse = handler.CommonResponseStatusInternalServerError("NOT IMPL")
-	}
-
+	commonResponse, _ := AnyCaller(c, "getuserinfo", commonRequest, true)
 	return c.Render(commonResponse.Status.StatusCode, r.JSON(commonResponse))
 }
