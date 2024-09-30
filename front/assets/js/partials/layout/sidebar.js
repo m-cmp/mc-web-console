ecd7815f9f3a56162b8893cb24f664f5f8f64b65
raccoon-mh <wsx134@gmail.com>
2024-09-30 05:20:03 +0000

    udpate sidebar active and delete unused html

--- front/assets/js/partials/layout/sidebar.js
+++ front/assets/js/partials/layout/sidebar.js
@@ -1,8 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     updatemenu();
-    setActiveMenu();
-    
     document.querySelectorAll('div[name^="sidebar_"]').forEach(function(item) {
         item.addEventListener('click', function() {
             const hrefStr = this.getAttribute('href')
@@ -11,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
+    setActiveMenu();
 });
 
 function updatemenu(){
@@ -20,8 +19,8 @@ function updatemenu(){
 }
 
 function generateMenuHTML(menus) {
-    // let html = ''; //<- 메뉴 완료될때까지 유지 
-    let html = document.getElementById("sidebar-menu-inner").innerHTML ;
+    let html = ''; //<- 메뉴 완료될때까지 유지 
+    // let html = document.getElementById("sidebar-menu-inner").innerHTML ;
     menus.forEach(title => {
         html += ` <li class="nav-item">`
         html += ` <div class="hr-text fs-3">${title.displayName}</div>`
@@ -41,7 +40,7 @@ function generateMenuHTML(menus) {
                         html += `<div class="dropdown-menu" name="sidebar_${menu.id}"><div class="dropdown-menu-columns">`;
                         menu.menus.forEach(subMenu => {
                             html += `<div class="dropdown-menu-column">`;
-                            html += `<a class="dropdown-item" href="/webconsole/${title.id}/${category.id}/${menu.id}/${subMenu.id}" id="sidebar_${subMenu.id}_${subMenu.id}">`;
+                            html += `<a class="dropdown-item" href="/webconsole/${title.id}/${category.id}/${menu.id}/${subMenu.id}" id="sidebar_${menu.id}_${subMenu.id}">`;
                             html += `${subMenu.displayName}</a>`;
                             html += `</div>`;
                         });
@@ -52,54 +51,61 @@ function generateMenuHTML(menus) {
             }
         });
     });
-
-    console.log("@@@@ menus", stringToBool(menus[0].isAction))
-
     return html;
 }
 
+function setActiveMenu() {
+    try {
+        const path = window.location.pathname.split('/');
+        console.log("path ", path);
+        const depth4 = path[4] ? `sidebar_${path[4]}` : null;
+        const depth5 = path[5] ? `sidebar_${path[4]}_${path[5]}` : null;
+        if (depth4) {
+            const elements = document.querySelectorAll(`[name="${depth4}"]`);
+            console.log(depth4, elements)
+            elements.forEach(i => {
+                if (!i.classList.contains('show')) i.classList.add('show');
+                if (!i.classList.contains('active')) i.classList.add('active');
+            });
+        }
+        if (depth5) {
+            const element = document.getElementById(depth5);
+            if (element && !element.classList.contains('active')) {
+                element.classList.add('active');
+            }
+        }
+    } catch (error) {
+        console.log('An error occurred in setActiveMenu:', error.message);
+    }
+}
 
 function stringToBool(str) {
     return str.toLowerCase() === 'true';
 }
 
-function setActiveMenu(){
-    try {
-        const path = window.location.pathname.split('/');
-        console.log("path ", path)
-        const depth2 = 'sidebar_' + path[3]
-        const depth3 = 'sidebar_' + path[3] + '_' + path[4]
-        console.log("depth2 ", depth2)
-        document.getElementsByName(depth2).forEach(i => i.classList.add('show', 'active'));
-        document.getElementById(depth3).classList.add('active');
-        } catch (error) {
-            console.log('An error occurred navbar.js:', error.message);
-        }
-}
-
-
-
 const iconsArr = {
+
     "workloads" : `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-    stroke-linejoin="round"
-    class="icon icon-tabler icons-tabler-outline icon-tabler-layout-dashboard">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M5 4h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1" />
-    <path d="M5 16h4a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-2a1 1 0 0 1 1 -1" />
-    <path
-        d="M15 12h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1" />
-    <path d="M15 4h4a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-2a1 1 0 0 1 1 -1" />
-</svg>`,
+        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+        stroke-linejoin="round"
+        class="icon icon-tabler icons-tabler-outline icon-tabler-layout-dashboard">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M5 4h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1" />
+        <path d="M5 16h4a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-2a1 1 0 0 1 1 -1" />
+        <path
+            d="M15 12h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1" />
+        <path d="M15 4h4a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-2a1 1 0 0 1 1 -1" />
+        </svg>`,
+
     "undefined" : `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-    stroke-linejoin="round"
-    class="icon icon-tabler icons-tabler-outline icon-tabler-layout-dashboard">
-    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-    <path d="M5 4h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1" />
-    <path d="M5 16h4a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-2a1 1 0 0 1 1 -1" />
-    <path
-        d="M15 12h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1" />
-    <path d="M15 4h4a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-2a1 1 0 0 1 1 -1" />
-</svg>`,
+        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+        stroke-linejoin="round"
+        class="icon icon-tabler icons-tabler-outline icon-tabler-layout-dashboard">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M5 4h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1" />
+        <path d="M5 16h4a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-2a1 1 0 0 1 1 -1" />
+        <path
+            d="M15 12h4a1 1 0 0 1 1 1v6a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1" />
+        <path d="M15 4h4a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1v-2a1 1 0 0 1 1 -1" />
+        </svg>`,
 }
