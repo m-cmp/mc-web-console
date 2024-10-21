a24c2a28dbf55f6b6c517ae5aec7a4865dd5a221
SungWoongz <ftsw0221@naver.com>
2024-10-21 17:06:54 +0900

    eventalarm develop

--- front/assets/js/common/api/services/eventalarm_api.js
+++ front/assets/js/common/api/services/eventalarm_api.js
@@ -8,3 +8,37 @@ export async function getAllPolicy() {
 
     return response
 }
+
+export async function getPolicyOfSeqHistory(policySeq) {
+    var data = {
+        queryParams: {
+            policySeq: policySeq,
+        },
+    };
+    var controller = "/api/" + "mc-observability/" + "ListUsingGET5";
+
+    const response = webconsolejs["common/api/http"].commonAPIPost(
+        controller,
+        data
+    );
+
+    return response
+
+}
+
+export async function getPolicyOfSeq(policySeq) {
+    var data = {
+        queryParams: {
+            policySeq: policySeq,
+        },
+    };
+    var controller = "/api/" + "mc-observability/" + "GetPolicyOfSeq";
+
+    const response = webconsolejs["common/api/http"].commonAPIPost(
+        controller,
+        data
+    );
+
+    return response
+
+}
--- front/assets/js/pages/operation/manage/eventalarm.js
+++ front/assets/js/pages/operation/manage/eventalarm.js
@@ -7,8 +7,7 @@ $("#select-current-project").on('change', async function () {
     webconsolejs["common/api/services/workspace_api"].setCurrentProject(project)// 세션에 저장
     console.log("select-current-project on change ", project)
     var respPolicyList = await webconsolejs["common/api/services/eventalarm_api"].getAllPolicy();
-    console.log("asdasd",respPolicyList)
-    var policyList = respPolicyList.responseData.data
+    var policyList = respPolicyList.data
     getPolicyListCallbackSuccess(project.NsId, policyList);
 })
 
@@ -24,6 +23,7 @@ var selectedWorkspaceProject = new Object();
 var policyListTable;
 var checked_array = [];
 export var selectedPolicyObj = new Object();
+var currentPolicySeq = "";
 
 
 
@@ -59,11 +59,24 @@ async function initEventAlarm() {
 }
 
 async function getPolicyListCallbackSuccess (nsId, policyList) {
-    totalPolicyListObj = policyList
-    setPolicyList(policyList)
+    
+    totalPolicyListObj = policyList.data.responseData.data
+    console.log("totalPolicyListObj",totalPolicyListObj)
+    const transformedData = mapPolicyData(totalPolicyListObj)
+    policyListTable.setData(transformedData);
+ 
 }
-async function setPolicyList () {
-  
+function mapPolicyData(data) {
+    return data.map(item => {
+        // threshold 필드를 파싱하여 crit, warn, info 필드를 추출
+        let threshold = JSON.parse(item.threshold || '{}');
+        return {
+            ...item,
+            crit: threshold.crit || '',
+            warn: threshold.warn || '',
+            info: threshold.info || ''
+        };
+    });
 }
 
 
@@ -193,29 +206,57 @@ function initPolicyTable() {
             field: "info",
             vertAlign: "middle",
         },
+        {
+            title: "Description",
+            field: "description",
+            visible: false,
+        },
     ];
 
     policyListTable = setPolicyTabulator("policylist-table", tableObjParams, columns, true);
 
     // 행 클릭 시
     policyListTable.on("rowClick", function (e, row) {
-        // vmid 초기화 for vmlifecycle
-        // selectedClusterId = ""
+        
+        var policySeq = row.getCell("seq").getValue();
 
-        var pmkID = row.getCell("id").getValue();
-
-        // 표에서 선택된 PmkInfo 
-        getSelectedPmkData(pmkID)
+        // 표에서 선택된 seqInfo 
+        getSelectedPolicySeqData(policySeq)
 
     });
 
     //  선택된 여러개 row에 대해 처리
     policyListTable.on("rowSelectionChanged", function (data, rows) {
         checked_array = data
-        selectedPmkObj = data
+        selectedPolicyObj = data
     });
     // displayColumn(table);
 }
+
+// 클릭한 pmk info 가져오기
+// 표에서 선택된 PmkId 받아옴
+async function getSelectedPolicySeqData(policySeq) {
+
+    console.log('selectedpolicySeq:', policySeq)
+    if (policySeq != undefined && policySeq != "") {
+        var selectedNsId = selectedWorkspaceProject.nsId;
+        currentPolicySeq = policySeq
+        var policySeqResp = await webconsolejs["common/api/services/eventalarm_api"].getPolicyOfSeqHistory(currentPolicySeq.toString())
+        
+        if (policySeqResp.status != 200) {
+            console.log("resp status ", policySeqResp.status)
+            // failed.  // TODO : Error Popup 처리
+            return;
+        }
+        // SET PMK Info page
+        setPolicyInfoData(policySeqResp.data)
+
+        // Toggle PMK Info
+        var div = document.getElementById("cluster_info");
+        webconsolejs["partials/layout/navigatePages"].toggleElement(div)
+    }
+}
+
 
 // toggleSelectBox of table row
 function toggleRowSelection(id) {
@@ -226,5 +267,65 @@ function toggleRowSelection(id) {
         console.log("Row with ID " + id + " is selected.");
     } else {
         console.log("Row with ID " + id + " not found.");
+    }
+}
+
+
+// 클릭한 pmk의 info값 세팅
+function setPolicyInfoData(policyData) {
+    console.log("setPolicyInfoData", policyData);
+
+    var clusterData = pmkData.responseData;
+    var clusterDetailData = clusterData.CspViewK8sClusterDetail;
+    var pmkNetwork = clusterDetailData.Network || {};
+
+    try {
+
+        var pmkName = clusterData.name;
+        var pmkID = clusterData.id
+        var pmkVersion = clusterDetailData.Version;
+        var pmkStatus = clusterDetailData.Status;
+
+        // 네트워크 정보
+        var pmkVpc = (pmkNetwork.VpcIID && pmkNetwork.VpcIID.SystemId) || "N/A";
+        var pmkSubnet = (pmkNetwork.SubnetIIDs && pmkNetwork.SubnetIIDs[0] && pmkNetwork.SubnetIIDs[0].SystemId) || "N/A";
+        var pmkSecurityGroup = (pmkNetwork.SecurityGroupIIDs && pmkNetwork.SecurityGroupIIDs[0] && pmkNetwork.SecurityGroupIIDs[0].SystemId) || "N/A";
+
+        // 추가정보
+        var pmkCloudConnection = clusterData.connectionName
+        var pmkEndPoint = clusterDetailData.AccessInfo.Endpoint
+        var pmkKubeConfig = clusterDetailData.AccessInfo.Kubeconfig // TODO: 너무 길어서 처리 질문
+
+        // webconsolejs["common/api/services/pmk_api"].getPmkInfoProviderNames(pmkData); // PMK에 사용된 provider
+        // var pmkDescription = clusterData.description;
+        // var pmkDispStatus = webconsolejs["common/api/services/pmk_api"].getPmkStatusFormatter(pmkStatus);
+        // var pmkStatusIcon = webconsolejs["common/api/services/pmk_api"].getPmkStatusIconFormatter(pmkDispStatus);
+        // var totalNodeGroupCount = (clusterDetailData.NodeGroupList == null) ? 0 : clusterDetailData.NodeGroupList.length;
+
+        $("#cluster_info_name").text(pmkName);
+        // $("#cluster_info_name").text(pmkName + " / " + pmkID);
+        $("#cluster_info_version").text(pmkVersion);
+        $("#cluster_info_status").text(pmkStatus);
+
+        // 네트워크 정보
+        $("#cluster_info_vpc").text(pmkVpc);
+        $("#cluster_info_subnet").text(pmkSubnet);
+        $("#cluster_info_securitygroup").text(pmkSecurityGroup);
+
+        // 추가정보
+        $("#cluster_info_cloudconnection").text(pmkCloudConnection);
+        $("#cluster_info_endpoint").text(pmkEndPoint || "N/A");
+        // $("#cluster_info_kubeconfig").text(pmkKubeConfig || "N/A");
+
+    } catch (e) {
+        console.error(e);
+    }
+
+    // TODO: pmk info로 cursor 이동
+    var nodeGroupList = clusterDetailData.NodeGroupList
+    
+    // displayNodeGroupStatusList(pmkID, clusterData)
+    if (Array.isArray(nodeGroupList) && nodeGroupList.length > 0) {
+        displayNodeGroupStatusList(pmkID, clusterData);
     }
 }
