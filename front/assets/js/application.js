46db0326a74fc7b95224ca09b1e991a8212edeb6
yh-noh <yhnoh@mz.co.kr>
2026-03-20 13:50:13 +0900

    refactor(jstree): compact row layout and fix CSS load order
    - jstree_for_tabler.scss를 application.js에서 jstree 테마 직후 require하도록 변경
      (application.scss import 제거 → CSS 우선순위 확보)
    - 행 높이 28px → 18px로 축소, flex row 레이아웃 적용
    - ocl/anchor/icon 각 요소 margin/padding 제거로 행 간격 최소화
    - wholerow 높이 고정, jstree-children flex-basis 100% 설정

--- front/assets/js/application.js
+++ front/assets/js/application.js
@@ -5,3 +5,5 @@ require("@fortawesome/fontawesome-free/js/all.js");
 require("jquery-ujs/src/rails.js");
 require("~jquery/dist/jquery.js");
 require("jstree/disk/themes/default/style.css");
+// jstree 오버라이드: 테마 직후 로드해 flex/행높이 등 우리 스타일이 적용되도록 함
+require("../css/jstree_for_tabler.scss");
