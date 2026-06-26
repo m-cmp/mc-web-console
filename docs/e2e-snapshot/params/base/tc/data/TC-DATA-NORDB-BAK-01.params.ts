2a5e66c579dfb3a4a825d0a680c91100bc89860a
yh-noh <yhnoh@mz.co.kr>
2026-06-26 15:53:23 +0900

    refactor(docs): docs/development 개발노트 및 docs/e2e-snapshot 제거
    - docs/development/bug, docs/development/fix: 임시 개발 노트 삭제 (내용은 PR/커밋 히스토리로 대체)
    - docs/e2e-snapshot: mcmp-e2e 레포로 이관 완료에 따라 mc-web-console에서 제거

--- docs/e2e-snapshot/params/base/tc/data/TC-DATA-NORDB-BAK-01.params.ts
+++
@@ -1,9 +0,0 @@
-import type { TCParams } from '../../../types';
-import { ALIBABA_NRDB, DATA_MANAGER_DEFAULTS } from './connections';
-
-export default {
-  base: {
-    ...DATA_MANAGER_DEFAULTS,
-    ...ALIBABA_NRDB,
-  },
-} satisfies TCParams;
--- docs/e2e-snapshot/params/base/tc/data/TC-DATA-OBJ-MIG-01.params.ts
+++
@@ -1,8 +0,0 @@
-import type { TCParams } from '../../../types';
-import { DATA_MANAGER_DEFAULTS } from './connections';
-
-export default {
-  base: {
-    ...DATA_MANAGER_DEFAULTS,
-  },
-} satisfies TCParams;
